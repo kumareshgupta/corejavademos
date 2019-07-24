7b0a3e59785f263ba13f5c21fbbc94628e2cef0f
kumareshg <kumareshg@M4L-FAC-KUMURS.mshome.net>
2019-07-25 00:00:09 +0530

    one more program added

---
+++ LambdaDemo/src/com/lambda/demo/LambdaDemo03.java
@@ -0,0 +1,19 @@
+package com.lambda.demo;
+
+interface IFunFour{
+	int multiply(int num1,int num2);
+}
+
+public class LambdaDemo03 {
+
+	public static void main(String[] args) {
+		IFunFour iRef = (num1, num2)->{
+			return (num1 * num2);
+		};
+		
+		int result = iRef.multiply(100, 200);
+		
+		System.out.println(result);
+	}
+
+}
